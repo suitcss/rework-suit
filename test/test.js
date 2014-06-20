bc96a2c04948f48935fcec0b43110c8192db0bb7
Nicolas Gallagher <nicolasgallagher@gmail.com>
2014-06-19 17:38:35 -0700

    Remove use of 'chai' in tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var expect = require('chai').expect;
+var assert = require('assert');
 var fs = require('fs');
 var rework = require('rework');
 var suit = require('../index');
@@ -10,7 +10,7 @@ describe('suit()', function () {
   var original = fs.readFileSync('test/fixtures/original.css', 'utf-8').toString().trim();
 
   it('generates the expected output', function () {
-    var processed = rework(original).use(suit).toString().trim();
-    expect(processed).to.equal(expected);
+    var actual = rework(original).use(suit).toString().trim();
+    assert.equal(actual, expected);
   });
 });
